a4343f21d474f07b95e528e22f6e22baca653120
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2024-04-09 14:20:41 +0300

    update ktor client

--- composeApp/src/commonMain/kotlin/api/Client.kt
+++ composeApp/src/commonMain/kotlin/api/Client.kt
@@ -1,15 +1,20 @@
 package api
 
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
-val httpClient = HttpClient() {
-    install(ContentNegotiation) {
-        json(Json {
-            ignoreUnknownKeys=true
-            prettyPrint=true
-        })
+class ApiClient {
+    val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    ignoreUnknownKeys=true
+                    prettyPrint=true
+                }
+            )
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/api/Repository.kt
+++ composeApp/src/commonMain/kotlin/api/Repository.kt
@@ -3,13 +3,30 @@ package api
 import data.CountModel
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.http.URLProtocol
+import io.ktor.http.path
 import kotlinx.coroutines.flow.flow
 
 class Repository {
-    suspend fun getCount() : CountModel {
-        val response = httpClient.get("https://api.edamam.com/api/recipes/v2?type=public&q=spagetti&app_id=93a8725b&app_key=0f746d5f5b07dc5dd1f954c71b8594ad")
-        return response.body()
-    }
+
+    private val client = ApiClient().httpClient
+
+    suspend fun getCount() : CountModel =
+        client.get("https://api.edamam.com/api/recipes/v2?type=public&q=spagetti&app_id=93a8725b&app_key=0f746d5f5b07dc5dd1f954c71b8594ad").body()
+
+    suspend fun getCountQuery(
+        query: String
+    ) : CountModel = client.get {
+            url {
+                protocol = URLProtocol.HTTPS
+                host = "api.edamam.com"
+                parameters.append("type", "public")
+                parameters.append("q", query)
+                parameters.append("app_id", "93a8725b")
+                parameters.append("app_key", "0f746d5f5b07dc5dd1f954c71b8594ad")
+                path("api/recipes/v2")
+            }
+        }.body()
 
     fun countFlow() = flow {
         emit(getCount())
